5c606352e49f77f14c786e798b8503c5ab882092
Maciej N <xyanteos@gmail.com>
2020-07-31 01:23:16 +0200

    Update

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import CardSelector from './CardSelector'
 import Cards from './Api'
+import ZliczPunkty from './ZliczPunkty'
 
-const request1 = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1", request2= "https://deckofcardsapi.com/api/deck/"
+const request1 = "https://deckofcardsapi.com/api/deck/"
 
 class App extends React.Component{
     state = {
@@ -10,52 +11,89 @@ class App extends React.Component{
         idDecka:null,
         posiadaneKarty:[],
         score: 0,
-        wygrana: false
+        wygrana: false,
+        zliczonoPkt: false
     }
-    //od razu po zaladowaniu strony wysylam zapytanie o decka
+    //dodaje bardzo potrzebny system do ZLICZANIA PKT... ;)
+    componentDidUpdate(){
+        if(this.state.posiadaneKarty.length>1 && !this.state.zliczonoPkt)
+        {
+             this.setState({score:<ZliczPunkty karty={this.state.posiadaneKarty}/>})
+             this.setState({zliczonoPkt:true})
+
+        }
+
+    }
+
+    //od razu po zaladowaniu strony wysylam zapytanie o decka (lub decki(?))
     componentDidMount(){
-        Cards.get(request1).then((res)=>{
-            console.log(res)
+        Cards.get(`${request1}new/shuffle/?deck_count=1`).then((res)=>{
+            //console.log(res)
             this.setState({idDecka:res.data.deck_id})
-            console.log(this.state.idDecka)
+            //console.log(this.state.idDecka)
         })
         .catch((err)=>{
             console.log(err)
         })
 
     }
-
+    //dodajemy opcje pobrania kolejnej karty z talii
+    dowalKarte=()=>{
+        console.log("dowalam")
+        Cards.get(`${request1}${this.state.idDecka}/draw/?count=1`)
+        .then((res)=>{
+            this.setState({posiadaneKarty:[...this.state.posiadaneKarty, res.data.cards[0]]})
+            this.setState({zliczonoPkt:false})
+            console.log(this.state.posiadaneKarty)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+    //Zmieniam tytuł h1 w zależności od etapu gry
+    coNapisac=()=>{
+        if(this.state.graRozpoczeta){
+            return(<div><h1>Gramy!</h1></div>)
+        }
+        else{
+            return(<div><h1>Kliknij, aby rozpocząć</h1></div>)
+        }
+    }
+    //czynności wykonywane podczas rozpoczęcia gry
     kliknietoKarty=()=>{
         //console.log(this.state.graRozpoczeta)
-        this.setState({graRozpoczeta:true})
-        if(!this.state.graRozpoczeta)
-        Cards.get(`${request2}${this.state.idDecka}/draw/?count=2`)
-        .then((res2)=>{
-            console.log(res2);
-            
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    }
+        
+        if(!this.state.graRozpoczeta){
+            this.setState({graRozpoczeta:true});
+            Cards.get(`${request1}${this.state.idDecka}/draw/?count=2`)
+            .then((res2)=>{
+                //onsole.log(res2);
+                this.setState({posiadaneKarty:res2.data.cards})
+            })
+            .catch((err)=>{
+                console.log(err)
+            });
+        }
 
+    }
+//                    {!this.state.graRozpoczęta ? <h1>Kliknij, aby rozpocząć!</h1> : <h1>Trwa rozgrywka...</h1>}
     render(){
         return(
             <div className="background">
                 <div className="oknoGry">
-                    {!this.state.graRozpoczęta ? <h1>Kliknij, aby rozpocząć!</h1> : <h1>Trwa rozgrywka...</h1>}
+
                     <div onClick={this.kliknietoKarty}>
-                        <CardSelector graRozpoczęta={this.state.graRozpoczeta} aktualnaKarta={this.state.aktualnaKarta} />
+                        {this.coNapisac()}
+                        <CardSelector graRozpoczęta={this.state.graRozpoczeta} posiadaneKarty={this.state.posiadaneKarty} />
                     </div>
                     <div className="scoreCounter">
-                        Your score: {this.state.score}
+                        Twój wynik: {this.state.score}
                     </div>
                     <div className="interakcja">
-                        <div className="dawaj">
-                            Hit me!
+                        <div className="dawaj" onClick={this.dowalKarte}>
+                            Dawaj!
                         </div>
                         <div className="koncz">
-                            Pass
+                            Stop!
                         </div>
                     </div>
                 </div>
--- src/Components/CardSelector.js
+++ src/Components/CardSelector.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import { render } from '@testing-library/react'
 
 
 const CardSelector = (props)=>{
-    if(props.graRozpoczęta)
+
+    //sprawdzam czy gra juz sie rozpoczela, czy nie
+    if(props.graRozpoczęta && props.posiadaneKarty.length>1)
     {
+        //jesli gra sie rozpoczela, to wyswietl karty wylosowane przez API
+        console.log(props)
+        const karty = props.posiadaneKarty.map((karta)=>{
+            return (<div><img src={karta.image} key={karta.code}/></div>)
+        })
         return(
-            <div className="karta">
-                <h2>Gramy!</h2>
+            <div className="karty">
+                {karty}
             </div>
         )
     }
+    //jesli gra sie nie rozpoczela, to pokazuj talię
     else{
         return(
-            <div className="karta">
+            <div>
                 <img src="talia.png"/>
             </div>
         )

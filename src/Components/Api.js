d0860a989c27d86dad909abdc8c022afb4c34cf7
Maciej N <xyanteos@gmail.com>
2020-09-07 02:07:03 +0200

    lel

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,3 +1,4 @@
 import Axios from 'axios'
-//zapytanie mogloby byc metodą z wejsciem ile kart lub talii dobrac i true/false czy nowa talia, czy dobieramy kartę
+//zapytanie mogloby byc metodą z wejsciem ile kart lub talii dobrac i true/false czy nowa talia, czy dobieramy kartę - taka poboczna dygresja
+
 export default Axios.create()
